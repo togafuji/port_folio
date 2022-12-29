7467c884148852dfecac9708408931bc4185f42a
togafuji <happy.tennis.ty.tm@gmail.com>
2022-12-29 08:37:32 +0000

    アカウント作成時の名前、画像の作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,8 @@ class ApplicationController < ActionController::Base
     end
   end
   
-  def after_destroy_user_session_path
+  def after_sign_out_path_for(resource)
+    flash[:notice] = "ログアウトしました" 
     families_path
   end
   
