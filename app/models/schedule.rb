cd395ffb947387a9311a6ff98d1b32d6410dca36
togafuji <happy.tennis.ty.tm@gmail.com>
2022-12-31 16:16:46 +0000

    検索のキーワード定義

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -6,4 +6,8 @@ class Schedule < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :whoschedule, presence: true
+  
+  def self.search(keyword)
+    where(["title like? OR content like? OR start_time like? OR whoschedule like?", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%", "%#{keyword}%"])
+  end
 end
