e68486b07472a8a10c4653b30f360a7ef3a99c27
togafuji <happy.tennis.ty.tm@gmail.com>
2023-01-03 13:13:29 +0000

    日記テーブル、コメントテーブル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_27_153041) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_02_152017) do
+  create_table "comments", force: :cascade do |t|
+    t.string "comment_content"
+    t.string "who_comment"
+    t.integer "user_id", null: false
+    t.integer "diary_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["diary_id"], name: "index_comments_on_diary_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "diaries", force: :cascade do |t|
+    t.text "body"
+    t.string "title"
+    t.string "avatar"
+    t.string "whodiary"
+    t.integer "user_id"
+    t.datetime "when_diary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "members", force: :cascade do |t|
     t.string "name：string"
     t.datetime "created_at", null: false
@@ -44,4 +66,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_27_153041) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "diaries"
+  add_foreign_key "comments", "users"
 end
