7dfc6076d55302f0108e4528249775e0afb8932e
togafuji <happy.tennis.ty.tm@gmail.com>
2023-01-10 16:43:51 +0000

    rspec導入により不要なファイルを削除

--- test/controllers/schedule_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ScheduleControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get schedule_index_url
-    assert_response :success
-  end
-end
